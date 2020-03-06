613e7b47836fefb489847fedf87cb9e6d788ea15
Shawn Guo <shawn.gou1@fiserv.com>
2020-03-06 17:01:06 +1300

    create new project

---
+++ TestDemo/TestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDemo
+//
+//  Created by Shawn Guo on 6/03/20.
+//  Copyright © 2020 Shawn Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
